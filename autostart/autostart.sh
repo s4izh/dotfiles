ae91e16000ae093ad1cf9217580e93619f530839
s4izh <sergiosanz234@gmail.com>
2022-08-16 00:09:31 +0200

    added folders to tmux-sessionizer

---
+++ autostart/autostart.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# systray battery icon
+# cbatticon -u 5 &
+# systray volume
+# volumeicon &
+setxkbmap es
+setxkbmap -option caps:escape
+picom --no-vsync -b
+feh --bg-fill /home/s4izh/Pictures/bg/1.png
+nm-applet &
