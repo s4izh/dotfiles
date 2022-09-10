9d2af2e045bf9a9fc006cc39ee72c336b9b3edc6
s4izh <sergiosanz234@gmail.com>
2022-09-10 18:55:16 +0200

    i3 wallpaper source changed

--- i3/.config/i3/autostart.sh
+++ i3/.config/i3/autostart.sh
@@ -4,6 +4,5 @@ setxkbmap es
 setxkbmap -option caps:escape
 
 picom -b
-feh --bg-fill /home/s4izh/Pictures/bg/archnord.png
-# (sleep 5; volumeicon) &
-
+feh --bg-fill /home/s4izh/Pictures/system-wallpaper
+(sleep 5; volumeicon) &
