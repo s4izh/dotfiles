0b34dee01ce1df0c22fde4c6fb99c9adb4d1400c
s4izh <sergiosanz234@gmail.com>
2022-10-01 22:42:00 +0200

    lightdm

---
+++ install/light-dm-install.sh
@@ -0,0 +1,5 @@
+# light-dm
+sudo pacman -S lightdm
+paru -S lightdm-webkit-theme-aether
+sudo sed -i 's/^webkit_theme\s*=\s*\(.*\)/webkit_theme = lightdm-webkit-theme-aether #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf
+sudo sed -i 's/^\(#?greeter\)-session\s*=\s*\(.*\)/greeter-session = lightdm-webkit2-greeter #\1/ #\2g' /etc/lightdm/lightdm.conf
