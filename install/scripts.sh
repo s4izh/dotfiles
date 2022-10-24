e33e39d833e38c79133350fb4d4c1861cf80e204
s4izh <sergiosanz234@gmail.com>
2022-10-24 20:10:30 +0200

    all

--- install/scripts.sh
+++ install/scripts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ln -s /home/s4izh/dotfiles/scripts/* /home/s4izh/.local/bin
-ln -s /home/s4izh/personal/dmenu/* /home/s4izh/.local/bin
+ln -s /home/s4izh/personal/dmenu/scripts/* /home/s4izh/.local/bin
