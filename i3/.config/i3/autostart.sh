af1530169199fcb7a93a1514593faf4dc7bb0552
s4izh <sergiosanz234@gmail.com>
2022-11-20 18:26:13 +0100

    autostart script

--- i3/.config/i3/autostart.sh
+++ i3/.config/i3/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 setxkbmap es -option caps:escape
-xinput --set-prop 11 291 1
+xinput --set-prop 11 305 0 1
 feh --bg-fill /home/s4izh/pictures/system-wallpaper
+polybar &
 sleep 3
-volumeicon
