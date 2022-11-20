60142a713b2887736a408dc14c3ca2e08471b478
s4izh <sergiosanz234@gmail.com>
2022-11-20 18:29:16 +0100

    new qtile config

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,12 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-# systray battery icon
-cbatticon -u 5 &
-# systray volume
-volumeicon &
-
-setxkbmap es
-setxkbmap -option caps:escape
-picom -b
-feh --bg-fill /home/s4izh/Pictures/system-wallpaper
+xrandr --output HDMI-A-0 --mode 1920x1080 --output eDP --off
+xinput --set-prop 15 308 0 1
+xinput --set-prop 10 305 0 1
+xinput --set-prop 11 305 0 1
+feh --bg-fill /home/s4izh/pictures/system-wallpaper
+picom --fade-in-step=1 --fade-out-step=1 --fade-delta=0 &
+setxkbmap es -option caps:escape &
 nm-applet &
